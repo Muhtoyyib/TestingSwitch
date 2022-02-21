c1c5756ad176f298c7e714f66abf921360343b14
Akande Olalekan Toheeb <akandeolalekantoheeb9@gmail.com>
2022-02-21 02:39:32 +0100

    Update script.js

--- file/script.js
+++ file/script.js
@@ -5,10 +5,12 @@ function myAns(ans){
    switch(ans){
        case 8:
             msg = 'Incorrect!';
+            document.getElementById("ans").style.color = "red";
         break;
 
         case 6:
             msg = 'Incorrect!';
+            document.getElementById("ans").style.color = "red";
         break;
 
         case 7:
@@ -18,6 +20,7 @@ function myAns(ans){
 
         case 10:
             msg = 'Incorrect!';
+            document.getElementById("ans").style.color = "red";
         break;
     }
 
